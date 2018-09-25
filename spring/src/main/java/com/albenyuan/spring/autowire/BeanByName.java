b845049f548577f0e18eb82fb3abf2d859c9eb18
Alben Yuan <albenyuan@aliyun.com>
2018-09-25 15:01:22 +0800

    @Autowire

---
+++ spring/src/main/java/com/albenyuan/spring/autowire/BeanByName.java
@@ -0,0 +1,20 @@
+package com.albenyuan.spring.autowire;
+
+import com.albenyuan.spring.bean.User;
+
+/**
+ * @Author Alben Yuan
+ * @Date 2018-09-06 17:04
+ */
+public class BeanByName {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ spring/src/test/java/com/albenyuan/spring/bean/TestAutowire.java
@@ -0,0 +1,16 @@
+package com.albenyuan.spring.bean;
+
+import com.albenyuan.spring.common.SpringBaseTestCase;
+import org.junit.Test;
+
+/**
+ * @Author Alben Yuan
+ * @Date 2018-09-06 17:12
+ */
+public class TestAutowire extends SpringBaseTestCase {
+
+    @Test
+    public void name() {
+
+    }
+}
